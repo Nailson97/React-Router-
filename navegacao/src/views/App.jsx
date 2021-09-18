69e3567b733e8638359466e394f6fd60d5290490
Nailson97 <nailsonsousa97@gmail.com>
2021-09-18 16:47:33 -0300

    commit inicial

---
+++ navegacao/src/views/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import React from 'react'
+
+export default function App (props) {
+    return (
+        <div className="App">
+            <h1>Component App</h1>
+        </div>
+    )
+}
